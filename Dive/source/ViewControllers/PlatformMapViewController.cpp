61daca66d28c73303a8946ac47c9c72680ef6a3d
Zach <zjs9@cornell.edu>
2019-03-18 12:40:46 -0400

    Hopefully working collisions

--- Dive/source/ViewControllers/PlatformMapViewController.cpp
+++ Dive/source/ViewControllers/PlatformMapViewController.cpp
@@ -5,26 +5,26 @@ void PlatformMapViewController::draw(shared_ptr<SpriteBatch> batch, shared_ptr<G
 void PlatformMapViewController::update(shared_ptr<GameState> state) {
     ///New changes--
     Keyboard* keyboard = Input::get<Keyboard>();
-    if (keyboard->keyPressed(KeyCode::ARROW_LEFT)) {
+    if (keyboard->keyDown(KeyCode::ARROW_LEFT)) {
         CULog("left");
         state->_map->parallaxTranslatePlatforms(-1);
-        for (int i = 0; i < _platforms.size(); i++) {
-            _platforms[i]->update(state);
-        }
     }
-    else if (keyboard->keyPressed(KeyCode::ARROW_RIGHT)) {
+    else if (keyboard->keyDown(KeyCode::ARROW_RIGHT)) {
         CULog("right");
         state->_map->parallaxTranslatePlatforms(1);
-        for (int i = 0; i < _platforms.size(); i++) {
-            _platforms[i]->update(state);
-        }
-    }
+	}
+	else {
+		state->_map->parallaxTranslatePlatforms(0);
+	}
     //previous code--
 //    state->_map->parallaxTranslatePlatforms(1);
 //    for (int i = 0; i < _platforms.size(); i++) {
 //        _platforms[i]->update(state);
 //    }
 	state->_map->rotatePlatforms();
+	for (int i = 0; i < _platforms.size(); i++) {
+		_platforms[i]->update(state);
+	}
 
 	//Camera Controller
 	Vec2 player_pos = state->_player->getPosition() * _grid_size;
@@ -32,12 +32,10 @@ void PlatformMapViewController::update(shared_ptr<GameState> state) {
 	float node_pos = -player_pos.y + _display.height / 2;
 	if (player_pos.y < _display.height/2) {
 		node_pos = 0;
-		CULog("a");
 	}
 	else if (player_pos.y > map_height - _display.height / 2) {
 		//Top portion
 		node_pos = -(map_height - _display.height);
-		CULog("b");
 	}
 	_node->setPositionY(node_pos);
 
